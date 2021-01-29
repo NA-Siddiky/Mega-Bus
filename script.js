1bbbeed84e7aa33055a6f7244489a37a9a43629f
NA-Siddiky <siddiky.academic@gmail.com>
2021-01-30 02:49:26 +0600

    adding first alternative Functional

--- script.js
+++ script.js
@@ -1,39 +1,60 @@
+// document.getElementById("firstClass-increase").addEventListener('click', function () {
+//     const firstClassInput = document.getElementById("firstClass-count");
+//     const firstClassCount = parseInt(firstClassInput.value);
+//     const firstClassNewCount = firstClassCount + 1;
+//     firstClassInput.value = firstClassNewCount;
+//     const firstClassTotal = firstClassNewCount * 150;
+//     document.getElementById('firstClass-total').innerText = '$' + firstClassTotal;
+
+// })
+
+// document.getElementById("firstClass-decrease").addEventListener('click', function () {
+//     const firstClassInput = document.getElementById("firstClass-count");
+//     const firstClassCount = parseInt(firstClassInput.value);
+//     const firstClassNewCount = firstClassCount - 1;
+//     firstClassInput.value = firstClassNewCount;
+//     const firstClassTotal = firstClassNewCount * 150;
+//     document.getElementById('firstClass-total').innerText = '$' + firstClassTotal;
+
+// })
+
+
+document.getElementById("firstClass-decrease").addEventListener('click', function () {
+    ticketHandler(false);
+})
 document.getElementById("firstClass-increase").addEventListener('click', function () {
+    ticketHandler(true);
+});
+
+function ticketHandler(isIncrease) {
     const firstClassInput = document.getElementById("firstClass-count");
     const firstClassCount = parseInt(firstClassInput.value);
-    const firstClassNewCount = firstClassCount + 1;
+    // const firstClassNewCount = firstClassCount + 1;
+    let firstClassNewCount = firstClassCount;
+    if (isIncrease == true) {
+        firstClassNewCount = firstClassCount + 1;
+    }
+    if (isIncrease == false) {
+        firstClassNewCount = firstClassCount - 1;
+    }
     firstClassInput.value = firstClassNewCount;
     const firstClassTotal = firstClassNewCount * 150;
     document.getElementById('firstClass-total').innerText = '$' + firstClassTotal;
 
-})
+}
 
-// document.getElementById("economyClass-increase").addEventListener('click', function () {
-//     const economyClassInput = document.getElementById("economyClass-count");
-//     const economyClassCount = parseInt(economyClassInput.value);
-//     const economyClassNewCount = economyClassCount + 1;
-//     economyClassInput.value = economyClassNewCount;
-//     const economyClassTotal = economyClassNewCount * 100;
-//     document.getElementById('economyClass-total').innerText = '$' + economyClassTotal;
+
+
+// document.getElementById("firstClass-decrease").addEventListener('click', function () {
+//     console.log('minus clicked');
 // })
-
-
-
-
-
-
-
-
-document.getElementById("firstClass-decrease").addEventListener('click', function () {
-    console.log('minus clicked');
-})
 // document.getElementById("firstClass-increase").addEventListener('click', function () {
-//     console.log('pluse clicked');
+//     console.log('pulse clicked');
 // } )
 
-document.getElementById("economyClass-decrease").addEventListener('click', function () {
-    console.log('minus clicked');
-})
+// document.getElementById("economyClass-decrease").addEventListener('click', function () {
+//     console.log('minus clicked');
+// })
 // document.getElementById("economyClass-increase").addEventListener('click', function () {
-//     console.log('pluse clicked');
+//     console.log('pulse clicked');
 // })
